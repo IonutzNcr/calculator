3fe16058b6145d53d683a5293f8c22816298372a
Ionut <ionutzncr@gmail.com>
2022-01-29 19:13:45 +0100

    Redit basic arithmetic function

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-function add (){
-    return "+"
+function add (a,b){
+    return a+b
 }
 
-function substract(){
-    return "-"
+function substract(a,b){
+    return a-b
 }
 
-function multiply(){
-    return "*"
+function multiply(a,b){
+    return a*b
 }
 
-const divide = function(){
-    return "/"
+ function divide(a,b){
+    return a/b
 }
 
-function operate(a,operator,b){
-    return eval(a + operator + b) 
-    
+function operate(a,b,operator){
+    switch (operator){
+        case "add" : return add(a,b);
+        
+        case "substract" : return substract(a,b);
+
+        case "divide" : return divide(a,b);
+
+        case "multiply" : return multiply(a,b);
+
+        default : console.log("you pick an undefined case")
+        
+
+    }
+   
 }
