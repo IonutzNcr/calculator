a2c99af17cbf827859b50a0a1ba03ad1bd2c16e7
Ionut <ionutzncr@gmail.com>
2022-01-29 18:55:44 +0100

    Add basic arithmetic function

---
+++ index.js
@@ -0,0 +1,20 @@
+function add (){
+    return "+"
+}
+
+function substract(){
+    return "-"
+}
+
+function multiply(){
+    return "*"
+}
+
+const divide = function(){
+    return "/"
+}
+
+function operate(a,operator,b){
+    return eval(a + operator + b) 
+    
+}
