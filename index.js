c4ed097376e326a206ee1ae366c58ecb5105f269
Ionut <ionutzncr@gmail.com>
2022-01-30 20:53:12 +0100

    Store result and numbers

--- index.js
+++ index.js
@@ -29,21 +29,45 @@ function operate(a,b,operator){
 }
 
 const buttons = document.querySelectorAll("button")
-const result = []
+const result = {
+    a:null,
+    b:null,
+    operator:null,
+}
+const number = []
 
 function displayCalculus(e){
-    
+    if(e.target.textContent=="="){
+        console.log("c bon")
+        return 1
+    }
     if(e.target.textContent=="C") {
         document.querySelector(".screen").textContent = "";
-        result.splice(0)
+        number.splice(0);
+        result.a = null;
+        result.b = null;
+        result.operator = null;
         return 0
+    }
+    if(e.target.textContent == "/" || e.target.textContent == "+" ||  e.target.textContent == "-" || e.target.textContent == "*"){
+        console.log(number)
+       result.a = +number.join("") 
+       console.log(result.a)
+       number.splice(0)
+        //if(result.a == null ) {
+       //     result.a = 
+       // }
+    }
+    if(e.target.textContent != "/" && e.target.textContent != "+" &&  e.target.textContent != "-" && e.target.textContent != "*"){
+        number.push(e.target.textContent)
+        console.log(number)
     }
     if(document.querySelector(".screen").textContent == "Calculus displayus"){
        document.querySelector(".screen").textContent = "";
     }
     document.querySelector(".screen").textContent += e.target.textContent
-    result.push(e.target.textContent)
-    console.log(result)
+    
+    
 }
 
 buttons.forEach(button=>button.addEventListener("click",displayCalculus))
