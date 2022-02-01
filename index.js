2cbd15b0f624bf709a9ff56b8ed191326a1124a9
Ionut <ionutzncr@gmail.com>
2022-02-01 21:41:11 +0100

    Rewrite the code

--- index.js
+++ index.js
@@ -29,123 +29,47 @@ function operate(a,b,operator){
 }
 
 const buttons = document.querySelectorAll("button");
-const result = {
+const calc = {
+    result :null,
     a:null,
     b:null,
     operator:null,
     isEqual:false,
+    isAllowed:false,
 };
+
 const number = [];
 
+function resetCalc(){
+    calc.result = null;
+    calc.a = null;
+    calc.b = null;
+    calc.operator = null;
+    calc.isEqual = false;
+    calc.isAllowed = false;
+}
+
 function displayCalculus(e){
+    
     if(document.querySelector(".screen").textContent == "Calculus displayus"){
         document.querySelector(".screen").textContent = "";
      }
+    if(e.target.textContent != "=" && e.target.textContent != "/" && e.target.textContent != "*" && e.target.textContent != "+" && e.target.textContent != "-" && e.target.textContent != "C" ){
+        number.push(e.target.textContent)
+        console.log(number)
+    } 
     
-     //working on the display
-     if(e.target.textContent == "=" && result.a == null){
-        console.log("sale")
-        return 0
-    }
-    
-    document.querySelector(".screen").textContent += e.target.textContent
-
-    //set b to the last number in the screen
-    //reset number array to empty array
-    //set a with operate() function 
-    // display the output ( which should be the final result)
-    //prevent the next digit to be display near the final result 
-    if(e.target.textContent=="="){
+    if(e.target.textContent == "=" || e.target.textContent == "/" || e.target.textContent == "*" || e.target.textContent == "+" || e.target.textContent == "-" ){
+        calc.isAllowed = false;
+        if (isAllowed == true) {
         
-        result.b = +number.join("")
-        number.splice(0)
-        console.log("a=",result.a,"b=",result.b)
-        const output = operate(result.a,result.b,result.operator);
-        document.querySelector(".screen").textContent = output;
-        result.a = output;
-        if(Number.isInteger(result.a)==false){
-            result.a = Math.round(result.a*10000)/10000 ;
-            console.log("a apres",result.a)
-            document.querySelector(".screen").textContent = result.a;
         }
-        result.operator = null; 
-        result.isEqual = true;
-        return 0
     }
-    // Screen reset to empty string
-    // Set the obj to its initial stage 
-    // Quit the function 
-    if(e.target.textContent == "C") {
-        document.querySelector(".screen").textContent = "";
-        number.splice(0);
-        result.a = null;
-        result.b = null;
-        result.operator = null;
-        result.isEqual = false;
-        return 0
-    }
-    if(e.target.textContent == "/" || e.target.textContent == "+" ||  e.target.textContent == "-" || e.target.textContent == "*"){
-      
-        // Prevent adding numbers on the screen after the equal sign
-        result.isEqual = false;
 
-        if(result.operator == null){
-            result.operator = e.target.id; 
-        }
-        
-       
-        // Initialize a when a is null!
-        // Reset the number array
-        // Leave the function once the condition actions are done 
-        if (result.a == null && result.b == null ) {
-           
-            result.a = +number.join("") 
-            number.splice(0)
-            return 0
-        }
-        //Initialize b w/ a number when b is null & a isn't null ! 
-        //Change  value of result.a  with operate function depending on the operator 
-        //reset number array to an empty array
-        //reset result.b to a null value 
+    //if(calc.result==null){
 
-        if (result.b == null && result.a != null ){
-            console.log(result.operator)
-            result.b = +number.join("")
-            result.a = operate(result.a,result.b,result.operator)
-            number.splice(0)
-            console.log("a",result.a)
-
-            //fix the decimal at 4 digits after the dot
-            if(Number.isInteger(result.a)==false){
-                result.a = Math.round(result.a*10000)/10000 ;
-                console.log("a apres",result.a)
-                document.querySelector(".screen").textContent = Math.round(result.a*10000)/10000 + e.target.textContent;
-            } else{ 
-                document.querySelector(".screen").textContent = result.a + e.target.textContent;
-                } 
-            
-            result.b = null;
-            result.operator = e.target.id; 
-            return 0
-        }
-        
-    }
-    //Tant que tu click sur un digit tu l'ajoute dans l'array number 
-    //reset B to null 
-    //Prevent to add numer to the final output 
-    //Allow to reset the calcul after entering a digit after clicking on equal
-    if(e.target.textContent != "/" && e.target.textContent != "+" &&  e.target.textContent != "-" && e.target.textContent != "*"){
-        
-        number.push(e.target.textContent);
-        result.b = null;
-        if(result.isEqual) {
-            document.querySelector(".screen").textContent = e.target.textContent;
-            result.a = null;
-            result.b = null;
-            result.operator = null;
-            result.isEqual = false;
-        }   
-    }   
+    //} 
+  
 }
 
 buttons.forEach(button=>button.addEventListener("click",displayCalculus))
--- psuedocode.md
+++ psuedocode.md
@@ -1,18 +1,52 @@
 PSEUDO CODE
 
-Take a number 
-    Take it from the screen 
-    ```JS   document.querySelector(".screen") ```
-Store the value inside a variable 
+Take two numbers 
+    Take a number 
+        If click on operator OR equal and no number stored 
+            Do nothing
+            Display nothing 
+            Wait for another event
+        Do this tant qu'il click dans sur un digit (NOT AFTER EQUAL)
+            Take the number 
+            Keep it 
+            Display it on the screen next to the previous if it exist
+        If click on an operator OR equal and there is a number 
+            Make a number with the previous numbers 
+            Keep the number
+            Keep the operator 
+            Display the operator next to the number 
+        If click on an operator or equal and there is two numbers stored 
+            Do the operation with the previous operator
+            Display the result 
+            Keep the result
+            Store the new operator 
+            Display the new operator next to the result
+        Else if click on an operator and there is already a result displayed
+            Operate the new number with the result 
+            Store the result 
+            Display the new result 
+        If click on an operator or equal and you already click on it (double click )
+            Do nothink
+            Display nothing
+            Wait for another event
+        If click on equal 
+            Operate with the current operator 
+            Store the result
+            Display the result
+            Store the new operator 
+            Operate with the previous operator 
+            Display the result 
+        If click on a digit after the equal sign was clicked
+            Reset everything
+        Else if Click on an operator after the equal sign was clicked 
+            Store the operator 
+            Wait for the next event 
+            
 
-Take another number 
-    Take it from the screen
+        Explain simple and briefly how the calculator should work
+    
+    The user will click on digits to form a number . The number is formed when the user will click on an operator except equal. Then he will click on another number then an equal or another operator. If h's clicking on equal the number will be formed and the result will be displayed. If h's clicking on another operateur the number is formed and the result will be done with the previous operator then displayed on the screen next to the current operator.
+    What is not allowed is to double click on operator. Or on equal. 
+    Apres un equal si il tape sur un digit tout se reset sinon la logic continue avec le resultat . 
 
-Store the value inside another variable
-
-Take an operator
-    Take it from the screen
-
-Store the value inside a value 
-
-Do the operation . 
+    
