a55020a797cc9708ffb6c6d1aee137b76a3d90a7
Ionut <ionutzncr@gmail.com>
2022-02-02 19:52:40 +0100

    Stuck in logic and coding

--- index.js
+++ index.js
@@ -29,8 +29,9 @@ function operate(a,b,operator){
 }
 
 const buttons = document.querySelectorAll("button");
+
 const calc = {
-    result :null,
+    result :false,
     a:null,
     b:null,
     operator:null,
@@ -41,12 +42,12 @@ const calc = {
 const number = [];
 
 function resetCalc(){
-    calc.result = null;
+    calc.result = false;
     calc.a = null;
     calc.b = null;
     calc.operator = null;
-    calc.isEqual = false;
-    calc.isAllowed = false;
+    calc.isEqual = false;   // click on equal is allowed
+    calc.isAllowed = false; // click on operator is allowed 
 }
 
 function displayCalculus(e){
@@ -54,22 +55,91 @@ function displayCalculus(e){
     if(document.querySelector(".screen").textContent == "Calculus displayus"){
         document.querySelector(".screen").textContent = "";
      }
-    if(e.target.textContent != "=" && e.target.textContent != "/" && e.target.textContent != "*" && e.target.textContent != "+" && e.target.textContent != "-" && e.target.textContent != "C" ){
-        number.push(e.target.textContent)
-        console.log(number)
-    } 
+     
     
-    if(e.target.textContent == "=" || e.target.textContent == "/" || e.target.textContent == "*" || e.target.textContent == "+" || e.target.textContent == "-" ){
-        calc.isAllowed = false;
-        if (isAllowed == true) {
+
+    if(e.target.textContent != "=" && e.target.textContent != "/" && e.target.textContent != "*" && e.target.textContent != "+" && e.target.textContent != "-" && e.target.textContent != "C" ){
+        if(document.querySelector(".screen").textContent == "Pick a number"){
+            document.querySelector(".screen").textContent = "";
+        }
+        if(calc.result==true){
+            document.querySelector(".screen").textContent = "";
+            resetCalc();
+        }
+        document.querySelector(".screen").textContent += e.target.textContent
+        number.push(e.target.textContent)
+        calc.isAllowed = true;
         
+        
+
+        if(calc.a != null) {
+            calc.isEqual = true;// c'est pas ici;
+                return 0
+        }
+        
+    } 
+    //working on equal 
+    if(e.target.textContent == "="){
+       
+        if(calc.isEqual == true){
+
+            calc.b = +number.join("")
+            number.splice(0)
+
+            calc.a = operate(calc.a,calc.b,calc.operator);
+            document.querySelector(".screen").textContent = calc.a;
+            //calc.b = null;
+
+            calc.result = true;
+            calc.isEqual = false;
+            calc.operator = null;
+            calc.isAllowed = true;
         }
     }
 
-    //if(calc.result==null){
+    if( e.target.textContent == "/" || e.target.textContent == "*" || e.target.textContent == "+" || e.target.textContent == "-" ){
+       
+        if(document.querySelector(".screen").textContent == "" ) {
+            document.querySelector(".screen").textContent = "Pick a number";
+            return 0
+        }
 
-    //} 
-  
+        if(calc.isAllowed == true){
+            calc.isAllowed = false;
+                // the problem is after the equal you can't use operator ;
+            if(calc.result == true){
+                calc.a = +document.querySelector(".screen").textContent
+                calc.operator = e.target.id;
+                document.querySelector(".screen").textContent = calc.a + e.target.textContent 
+                //calc.result = false; //il faut pas le mettre ici
+                console.log(calc.a , calc.b, calc.operator)
+                //return 0
+            }
+
+            if(calc.a==null){
+                calc.a = +number.join("");
+                calc.operator = e.target.id;
+                number.splice(0);
+
+                document.querySelector(".screen").textContent = calc.a + e.target.textContent;
+                return 0
+            }
+
+            if(calc.b==null){
+                calc.b = +number.join("");
+                console.log(calc.b)
+                number.splice(0);
+                calc.a = operate(calc.a,calc.b,calc.operator);
+               
+                calc.operator = e.target.id ;
+                document.querySelector(".screen").textContent = calc.a + e.target.textContent;
+                calc.b = null;
+                
+            }
+        }
+         
+       
+    }
 }
 
 buttons.forEach(button=>button.addEventListener("click",displayCalculus))
--- psuedocode.md
+++ psuedocode.md
@@ -1,52 +1,47 @@
-PSEUDO CODE
+PSEUDO CODE ; 
 
-Take two numbers 
-    Take a number 
-        If click on operator OR equal and no number stored 
-            Do nothing
-            Display nothing 
-            Wait for another event
-        Do this tant qu'il click dans sur un digit (NOT AFTER EQUAL)
-            Take the number 
-            Keep it 
-            Display it on the screen next to the previous if it exist
-        If click on an operator OR equal and there is a number 
-            Make a number with the previous numbers 
-            Keep the number
-            Keep the operator 
-            Display the operator next to the number 
-        If click on an operator or equal and there is two numbers stored 
-            Do the operation with the previous operator
-            Display the result 
-            Keep the result
-            Store the new operator 
-            Display the new operator next to the result
-        Else if click on an operator and there is already a result displayed
-            Operate the new number with the result 
-            Store the result 
-            Display the new result 
-        If click on an operator or equal and you already click on it (double click )
-            Do nothink
-            Display nothing
-            Wait for another event
-        If click on equal 
-            Operate with the current operator 
-            Store the result
-            Display the result
-            Store the new operator 
-            Operate with the previous operator 
-            Display the result 
-        If click on a digit after the equal sign was clicked
-            Reset everything
-        Else if Click on an operator after the equal sign was clicked 
-            Store the operator 
-            Wait for the next event 
-            
+Prend un nombre 
+Stock le dans une variable 
 
-        Explain simple and briefly how the calculator should work
-    
-    The user will click on digits to form a number . The number is formed when the user will click on an operator except equal. Then he will click on another number then an equal or another operator. If h's clicking on equal the number will be formed and the result will be displayed. If h's clicking on another operateur the number is formed and the result will be done with the previous operator then displayed on the screen next to the current operator.
-    What is not allowed is to double click on operator. Or on equal. 
-    Apres un equal si il tape sur un digit tout se reset sinon la logic continue avec le resultat . 
+Prend un operateur 
+Stock le dans une variable
 
-    
+Prend un autre nombre 
+Stock le dans une variable 
+
+Operate (a,b,operator)
+Display the result 
+
+
+Quand on click sur nombre 
+    store le chiffre dans l'array 
+    isAllowed = true 
+    if(operator != null)
+        Alors isEqual == true
+
+Quand t'appuie sur un operator 
+    if(isAllowed = true)
+        Si a == nul 
+            Alors transforme les chiffre en un numero et stock le dans a 
+            Et sauvegarde l'operateur
+        Else if b == nul 
+            Sauvegarde l'operator 
+        Else a != null & b != null 
+            Alors transforme  les chiffres en un numero et stock le dans b 
+            Assign a la valeur de operate (a,b,ancien operator)
+            operator = current operator
+     
+
+        Is equal = false // tu ne peux pas utiliser equal apres un operator
+        isAllowed = false //  peux pas utiliser un  operator apres un operator
+    Sinon 
+        Rien
+
+Quand t'appuie sur equal sign 
+
+    If(isEqual == true){
+        calc.a = operate(a,b, operator)
+        isEqual == false 
+        isResult == true;
+    }
+
