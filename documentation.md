bcda0f09cc5a2e575b855a6dcd9bc2c5039f5050
Ionut <ionutzncr@gmail.com>
2022-03-04 15:19:57 +0100

    Add AC button + C button + Fixing abusiv behavior

--- documentation.md
+++ documentation.md
@@ -1,14 +1,50 @@
-Regulator : calc.equalAbuser  ==> equal can't be clicked if is true . 
-            calc.operatorAbuser ==> operator can't be clicked if is true. 
 
-            ==> prevent the abuse of these operators. 
-
-            Is true when there is no number on the screen or saved in a variable;
-            Is true when there is already an operator on the screen
-
-About logic . 
-
-Function that will save the number when an operator is pressed. 
     
 
-I do really need to understand regExp to prevent writing too much 
+I do really need to understand regExp to prevent writing too much code in vain;
+
+------------------------------------------------------------------------------
+Explication : 
+
+calc.allowedEqual = false; means that there is not possible to use equal 
+
+calc.allowedEqual = true; means that you can use equal  to fulfil its purpose. 
+
+allowedEqual should be false: - when there isn't any number on the screen;
+                              - when you clicked on equal 
+                              - when you clicked on an operator
+
+allowedEqual should be true : - wehn you clicked on a digit 
+
+--------------------------------------------------------------------------
+calc.allowedOperator = false; means that you can't use any operator 
+
+calc.allowedOperator = true; means that you can use  operators
+
+allowedOperator should be false: - when there isn't any number on the screen
+                                 - when you already clicked on an operator
+
+allowedOperator should be true : - when you clicked on a digit
+                                 - after the result of an operation using equal
+
+-------------------------------------------------------------------------------------
+allowedReset = false;  means it will nos reset on clicking on a digit
+
+allowedReset = true; means it will reset clicking on a digit
+
+allowedReset should be false when : 
+                                    - after you clicked on an operator
+
+resetCalc() should be used : -After the result of an operation using equal if you click on    a digit                       
+                             - Using AC button
+
+--------------------------------------------------------------------------------------------
+C BUTTON :  Remove a number from the number array if there is any 
+
+-------------------------------------------------------------------------------------------
+
+allowedDot = true means that u can use the dot
+
+allowedDot = false means u can't use the dot
+
+allowedDot should be true when 
--- index.js
+++ index.js
@@ -35,9 +35,9 @@ const calc = {
     a:null,
     b:null,
     operator:null,
-    operatorAbuser:true,
-    equalAbuser:true,
-    isEqual:false,
+    allowedEqual:false,
+    allowedOperator : false,
+    allowedReset : false,
 };
 
 const number = [];
@@ -47,9 +47,10 @@ function resetCalc(){
     calc.a = null;
     calc.b = null;
     calc.operator = null;
-    calc.operatorAbuser = true;
-    calc.equalAbuser = true;
-    calc.isEqual = false; /* define if an operation was done using equal */
+    calc.allowedEqual = false; /* define if an operation was done using equal 
+    + define if we can use an operator (because if we can use equal we can also use an operator))*/
+    calc.allowedOperator = false;
+    calc.allowedReset = false;
 };
 
 buttons.forEach(button=>button.addEventListener("click",displayCalculus))
@@ -57,42 +58,70 @@ buttons.forEach(button=>button.addEventListener("click",displayCalculus))
 function displayCalculus(e){
     resetScreen();
 
+    if(e.target.textContent.match(/[C]/)){
+        resetCalc()
+        document.querySelector(".screen").textContent = "Welcome!" 
+        number.splice(0)
+        return 
+        
+    }
+
+    if(e.target.textContent.match(/[AC]/)){
+        number.pop()
+        document.querySelector(".screen").textContent = number.join("");
+
+    }
     /* When the user click on a digit:
     Put the digit inside the array,
     Display the the previous content on the screen and the current content
     */
     if(e.target.textContent.match(/[0-9]/)){
-        if(calc.isEqual = true)
-        {
-            resetCalc()
+        /* When a result is displayed using equal sign and then the user click on a digit:
+        the outcome to be expected is to reset everthing */
+        if(calc.allowedReset==true){
+            resetCalc();
+            calc.allowedReset = false;
         }
+        calc.allowedOperator = true;
+        calc.allowedEqual = true;
         number.push(e.target.textContent);
         document.querySelector(".screen").textContent = number.join("");
     }
 
     /* When user click on equal */
     if(e.target.textContent.match(/[=]/)){
-        calc.isEqual = true
-        if(calc.a==null){
-            calc.a = +number.join("")
-            number.splice(0)
-            document.querySelector(".screen").textContent = calc.a;
-        }
 
-        if(calc.a != null){
-            calc.b = +number.join("")
-            number.splice(0)
-            calc.a = operate(calc.a,calc.b,calc.operator)
-            calc.b = null
-            document.querySelector(".screen").textContent = calc.a
+        if(calc.allowedEqual == true){
+            calc.allowedEqual = false;
+            calc.allowedOperator = true;
+            calc.allowedReset = true;
+            if(calc.a==null){
+                calc.a = +number.join("")
+                number.splice(0)
+                document.querySelector(".screen").textContent = calc.a;
+                console.log("a==nul")
+                
+                
+                return 
+            }
+    
+            if(calc.a != null){
+                calc.b = +number.join("")
+                number.splice(0)
+                calc.a = operate(calc.a,calc.b,calc.operator)
+                calc.b = null
+                document.querySelector(".screen").textContent = calc.a
+                console.log("a!=nul")
+            }
+            
         }
+       
     }
 
     /* 
     When the user click  on an operator (equal is not included):
     */
     if( e.target.textContent.match(/[*/+-]/) ){
-        calc.isEqual = false;
         /*  If there is not a number created by the array:
         Create a nuberd inside calc.a ,
         Empty the array ,
@@ -112,7 +141,6 @@ function displayCalculus(e){
         Add the result of a operate b in calc.a using the previous operator,
         Disply calc.a next to the operator , 
         Save the new operator inside calc.operator ,
-        
         */
         if(calc.a!=null && calc.operator != null){
         
@@ -121,10 +149,12 @@ function displayCalculus(e){
         number.splice(0);
         calc.a= operate(calc.a,calc.b,calc.operator)
         document.querySelector(".screen").textContent = calc.a + e.target.textContent
-        
         calc.operator = e.target.textContent
+
         }
-        
+        calc.allowedEqual = false;
+        calc.allowedOperator = false;
+        calc.allowedReset = false;
     }
 }
 
@@ -133,10 +163,5 @@ function resetScreen(){
     if( document.querySelector(".screen").textContent == "Welcome!" ) {
         document.querySelector(".screen").textContent = "";
     }
-
-    /* if(!document.querySelector(".screen").textContent){
-        console.log("maria")
-        document.querySelector(".screen").textContent = " ";
-    } */
 }
 
